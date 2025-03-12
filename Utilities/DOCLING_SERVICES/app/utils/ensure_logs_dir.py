8396e3f91dbf84d3deec52b4b6ff170eaafac411
PrakharG-kore <prakhar.gupta@kore.com>
2025-03-12 12:25:15 +0530

    Delete Utilities/DOCLING_SERVICES directory

--- Utilities/DOCLING_SERVICES/app/utils/ensure_logs_dir.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def ensure_logs_directory():
-    """Ensure that the logs directory exists."""
-    logs_dir = 'logs'
-    if not os.path.exists(logs_dir):
-        os.makedirs(logs_dir) 
